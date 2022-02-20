4b4160e8e96c497c883ad20dd0e129956ddca930
emunac@gmail.com <emunac@gmail.com>
2022-02-20 16:25:27 +0200

    minor_changes

--- main.c
+++ main.c
@@ -5,9 +5,9 @@ struct point {
     int y;
 };
 
-int in_grid(x, y){
+int in_grid(int x, int y){
 
-    if(x<0 || x>7 || y<0 || y>7){
+    if(x < 0 || x > 7 || y < 0 || y > 7){
         return 0;
     }
     return 1;
@@ -15,14 +15,14 @@ int in_grid(x, y){
 
 struct point next_available_step(int x, int y, int grid[8][8]){
 
-    int next_x[4] = {x-2, x+2, x-1, x+1};
-    int next_y[4] = {y-2, y+2, y-1, y+1};
+    int next_x[4] = { x-2, x+2, x-1, x+1 };
+    int next_y[4] = { y-2, y+2, y-1, y+1 };
     int i,j;
     struct point ret = {-1, -1};
 
-    for(i=0;i<4;i++){
+    for(i = 0; i < 4; i++){
         for(j = 0; j < 4; j++){
-            if((i<2 && j<2) || i>=2 && j>=2){
+            if((i < 2 && j < 2) || i >= 2 && j >= 2){
                 continue;
             }
             ret.x = next_x[i];
@@ -41,10 +41,9 @@ struct point next_available_step(int x, int y, int grid[8][8]){
 int knight_walk(int x, int y){
 
     int count = 0;
-    int grid[8][8] = {0};
+    int grid[8][8] = { 0 };
     struct point next;
 
-    // 0 <= x,y <= 7
     x -= 1;
     y -= 1;
     if(!in_grid(x, y)){
@@ -55,14 +54,14 @@ int knight_walk(int x, int y){
     grid[x][y] = 1;
 
     while(count <= 64){
-        next = next_available_step(x, y,grid);
+        next = next_available_step(x, y, grid);
         if(next.x == -1){ // No more available steps
             return count;
         }
         count ++;
         x = next.x;
         y = next.y;
-        printf("Step %d:(%d, %d)\n", count, x, y);
+        printf("Step %d: (%d, %d)\n", count, x, y);
         grid[x][y] = 1;
     }
 }
